97b60cb60f741167a3c7a30954e92c831abbdabd
tejaswini16-m <98205166+tejaswini16-m@users.noreply.github.com>
2022-01-22 12:39:51 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+p{
+  font-family: garamond;
+  font-size: 30px;
+}
+p:nth-child(odd){
+  color:  rgb(10,10,10);
+}
+
+</style>
+
+<body ><center>
+  <?php include ("nav.php"); ?>
+  <b>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <p> &nbsp; &nbsp; &nbsp; To be the 'Most Admired Bank' which is</p>
+    <p> &nbsp; &nbsp; &nbsp; Digitally enabled with a sharp focus on Micro,</p>
+    <p> &nbsp; &nbsp; &nbsp; Medium and Middle market enterprise.</p>
+  </b>
+  <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+
+
+
+  <?php include('footer.php') ?>
+</center>
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,4 @@
+﻿<div id="container">
+    <div id="header">© 2022. Made by Tejaswini M <br>
+      For the Project of The Sparks Foundation</div>
+ </div>
